92d24e033349d0b1ec848f4488bfddbeff7718bc
KhanhGCH200087 <khanhpmgch200087@fpt.edu.vn>
2022-08-15 15:25:06 +0700

    Change entity teacher

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,17 +2,103 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
+use App\Form\TeacherType;
+use App\Repository\TeacherRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/teacher')]
 class TeacherController extends AbstractController
 {
-    #[Route('/teacher', name: 'app_teacher')]
-    public function index(): Response
+    #[Route('/index', name: 'teacher_index')]
+    public function teacherIndex()
     {
-        return $this->render('teacher/index.html.twig', [
-            'controller_name' => 'TeacherController',
+        $teachers = $this->getDoctrine()->getRepository(Teacher::class)->findAll();
+        return $this->render('teacher/index.html.twig', 
+        [
+            'teachers' => $teachers
         ]);
+    }
+
+    #[Route('/detail/{id}', name:'teacher_detail')]
+    public function teacherDetail ($id, TeacherRepository $teacherRepository) 
+    {
+        $teacher = $teacherRepository->find($id);
+        if ($teacher == null)
+        {
+            $this ->addFlash('Warning', 'Invalid teacher $id !');
+            return $this->redirectToRoute('teacher_index');
+        }
+        return $this->render('teacher/detail.html.twig', 
+        [
+            'teacher'=>$teacher
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'teacher_delete')]
+    public function teacherDelete ($id, ManagerRegistry $managerRegistry)
+    {
+        $teacher = $managerRegistry->getRepository(Teacher::class)->find($id);
+        if($teacher == null){
+            $this ->addFlash('Warning', 'Teacher is not existed');
+            return $this->redirectToRoute('teacher_index');
+        } else {
+            $manager = $managerRegistry->getManager();
+            $manager -> remove($teacher);
+            $manager ->flush();
+            $this->addFlash('Info', 'Delete book successfully');
+        }
+    }
+
+    #[Route('/add', name:'teacher_add')]
+    public function teacherAdd (Request $request)
+    {
+        $teacher = new Teacher();
+        $form = $this->createForm(TeacherType::class, $teacher);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($teacher);
+            $manager->flush();
+            $this->addFlash('Info', 'New teacher is added successfully');
+            return $this->redirectToRoute('teacher_index');
+        }
+
+        return $this->renderForm('teacher/add.html.twig', 
+        [
+            'teacherForm' => $form
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'teacher_edit')]
+    public function teacherEdit ($id, Request $request)
+    {
+        $teacher = $this->getDoctrine()->getRepository(Teacher::class)->find($id);
+        if($teacher == null)
+        {
+            $this->addFlash('Warning', 'Wrong ID');
+            return $this->redirectToRoute('teacher_index');
+        } else {
+            $form = $this->createForm(TeacherType::class, $teacher);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($teacher);
+                $manager->flush();
+                $this->addFlash('Info', 'Add book successfully');
+                return $this->redirectToRoute('teacher_index');
+            }
+            return $this->renderForm('teacher/edit.html.twig', 
+            [
+                'teacherForm' => $form
+            ]);
+        }
     }
 }
--- src/Form/TeacherType.php
+++ src/Form/TeacherType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +14,51 @@ class TeacherType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('Teacher_name', TextType::class, 
+            [
+                'required' => true, 
+                'requiredMessage' => 'Please enter teacher name',
+                'label' => 'Teacher name', 
+                'attr' => [
+                    'minlength' => 2,
+                ]
+            ])
+
+            ->add('Teacher_age', TextType::class, 
+            [
+                'required' => true, 
+                'requiredMessage' => 'Please enter age',
+                'label' => 'Teacher age',
+                'attr' => [
+                    'min' => 20,
+                    'minMessage' => 'Age must at least 20',
+                    'max' => 80,
+                    'maxMessage' => 'Age must less than 81',
+                ]
+            ]) 
+
+            ->add('Teacher_gender', TextType::class, 
+            [
+                'required' => true, 
+                'requiredMessage' => 'Please enter gender of teacher',
+                'label' => 'Teacher gender',
+            ]) 
+
+            ->add('Teacher_email', EmailType::class, 
+            [
+                'required' => true, 
+                'requiredMessage' => 'Please enter email',
+                'label' => 'Teacher email',
+                'email' => true,
+            ]) 
+
+            ->add('Teacher_pass', PasswordType::class, 
+            [
+                'required' => true, 
+                'requiredMessage' => 'Please enter password',
+                'label' => 'Teacher password',
+            ]) 
+            
         ;
     }
 
